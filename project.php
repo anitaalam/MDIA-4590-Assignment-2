49f6f48497cdab5d217f0a498d489364c153556a
anitaalam <anitawingyeelam@gmail.com>
2022-07-12 00:55:42 -0700

    Styling changes
    Making styling changes on portfolio

--- project.php
+++ project.php
@@ -1,35 +1,46 @@
 <section id="projects">
     <h2>Projects</h2>
     <div class="card_container">
-        <div class="project_card">
-            <img class="project" src="images/thebujoguide.jpg" alt="Web development project on a guide about bullet journaling.">
-            <div class="card_overlay">
-                <div class="projectitle">THE BUJO GUIDE</div>
-                <div class="view-btn">
-                    <a href="artifact01/index.html" target="_blank">View Site</a>
-                    <a href="https://github.com/anitaalam/the-bujo-guide" target="_blank">View Code</a>
+        <div class="project-card-wrapper">
+            <div class="project_card">
+                <img class="project" src="images/thebujoguide.jpg" alt="Web development project on a guide about bullet journaling.">
+                <div class="card_overlay">
+                    <!-- <div class="projectitle">THE BUJO GUIDE</div> -->
+                    <div class="view-btn">
+                        <a class="view-site" href="artifact01/index.html" target="_blank">View Site</a>
+                        <a class="view-code" href="https://github.com/anitaalam/the-bujo-guide" target="_blank">View Code</a>
+                    </div>
                 </div>
             </div>
+            <div class="projectitle">THE BUJO GUIDE</div>
+            <p class="project-description">A guide to bullet journaling &verbar; Responsive website</p>
         </div>
-        <div class="project_card">
-            <img class="project" src="images/mp3player.png" alt="MP3 Player creating using HTML, CSS, and JavaScript.">
-            <div class="card_overlay">
-                <div class="projectitle">MP3 PLAYER</div>
-                <div class="view-btn">
-                    <a href="artifact02/index.html" target="_blank">View Site</a>
-                    <a href="https://github.com/anitaalam/mp3-player" target="_blank">View Code</a>
+        <div class="project-card-wrapper">
+            <div class="project_card">
+                <img class="project" src="images/mp3player.png" alt="MP3 Player creating using HTML, CSS, and JavaScript.">
+                <div class="card_overlay">
+                    <!-- <div class="projectitle">MP3 PLAYER</div> -->
+                    <div class="view-btn">
+                        <a class="view-site" href="artifact02/index.html" target="_blank">View Demo</a>
+                        <a class="view-code" href="https://github.com/anitaalam/mp3-player" target="_blank">View Code</a>
+                    </div>
                 </div>
             </div>
+            <div class="projectitle">MP3 PLAYER</div>
+            <p class="project-description">Korean Playlist &verbar; HTML &verbar; CSS &verbar; JavaScript</p>
         </div>
-        <div class="project_card">
-            <img class="project" src="images/animations.jpg" alt="Animations created using HTML,CSS, JavaScript, and GSAP.">
-            <div class="card_overlay">
-                <div class="projectitle">ANIMATIONS</div>
-                <div class="view-btn">
-                    <a href="artifact03/index.html" target="_blank">View Site</a>
-                    <a href="https://github.com/anitaalam/animations" target="_blank">View Code</a>
+        <div class="project-card-wrapper">
+            <div class="project_card">
+                <img class="project" src="images/animations.jpg" alt="Animations created using HTML,CSS, JavaScript, and GSAP.">
+                <div class="card_overlay">
+                    <div class="view-btn">
+                        <a class="view-site" href="artifact03/index.html" target="_blank">View Demo</a>
+                        <a class="view-code" href="https://github.com/anitaalam/animations" target="_blank">View Code</a>
+                    </div>
                 </div>
             </div>
+            <div class="projectitle">ANIMATIONS</div>
+            <p class="project-description">Collections of animations &verbar; JavaScript &verbar; GSAP</p>
         </div>
     </div>
 </section>
