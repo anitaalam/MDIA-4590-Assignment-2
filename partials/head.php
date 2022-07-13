984d4d7b78bb83ab6c869bac8bd4849761c8043d
anitaalam <anitawingyeelam@gmail.com>
2022-07-12 18:37:24 -0700

    Adding metadata tag to head.php
    adding metadata tags to make it easier to search for my portfolio

--- partials/head.php
+++ partials/head.php
@@ -1,8 +1,11 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- GOOGLE FONTS -->
 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:ital,wght@0,400;1,300&display=swap" rel="stylesheet">
+<!-- STYLESHEETS -->
 <link rel="stylesheet" href="styles/reset.css">
 <link rel="stylesheet" href="styles/main.css">
+<!-- FAVICON -->
 <link rel="apple-touch-icon-precomposed" sizes="57x57" href="images/favicon/apple-touch-icon-57x57.png" />
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/favicon/apple-touch-icon-114x114.png" />
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/favicon/apple-touch-icon-72x72.png" />
@@ -23,3 +26,9 @@
 <meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
 <meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
 <meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
+<!-- METADATA TAGS -->
+<meta name="description" content="Anita Lam is a Front-end Developer. She is currently attending the New Media Design and Web Development Program at BCIT and is looking forward to starting an internship in Fall 2022.">
+<meta name="og:title" content="Anita Lam | Vancouver Front-end Developer">
+<meta name="og:site_name" content="Anita Lam Portfolio">
+<meta name="og:url" content="https://anitalam.ca">
+<meta name="og:image" content="images/anitalam.jpg">
